9aaf6e2150817bcd97ebaac274acb00d23c98901
mo171 <movindsouza79@gmail.com>
2026-02-15 17:17:02 +0530

    frontend-intilize

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button.jsx";
+
+function page() {
+  return (
+    <div>
+      page <Button variant="primary">Button</Button>
+    </div>
+  );
+}
+
+export default page;
